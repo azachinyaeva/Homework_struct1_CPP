824c72f5dd84cb3c6dd57a1eb15c784d05a6883c
Anna <annahasagun@yahoo.com>
2023-01-26 16:51:28 +0300

    Initial commit

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+struct bank_account {
+    int account_num;
+    string name;
+    float balance;
+};
+void change(bank_account& account, int new_balance) {
+    account.balance = new_balance;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    bank_account account;
+    int new_balance;
+    cout << "Введите номер счёта:  ";
+    cin >> account.account_num;
+    cout << "Введите имя владельца: ";
+    cin >> account.name;
+    cout << "Введите баланс: ";
+    cin >> account.balance;
+    cout << "Введите новый баланс: ";
+    cin >> new_balance;
+    change(account, new_balance);
+    cout << "Ваш счёт: " << account.account_num << ", " << account.name << ", " << account.balance;
+    return 0;
+}
